9150d41081b73ba5d84e710b0ad382dcc3c22b55
Liam Nichol <Liam.Nichol@gmx.com>
2024-05-30 11:10:53 +0100

    FreshBoxedProduct is now sealed to prevent repackaging of its content

--- BethanysPieShop.InventoryManagement/Domain/ProductManagement/FreshBoxedProduct.cs
+++ BethanysPieShop.InventoryManagement/Domain/ProductManagement/FreshBoxedProduct.cs
@@ -2,7 +2,7 @@
 
 namespace BethanysPieShop.InventoryManagement.Domain.ProductManagement
 {
-    public class FreshBoxedProduct : BoxedProduct
+    public sealed class FreshBoxedProduct : BoxedProduct
     {
         public FreshBoxedProduct(int id, string name, string? description, Price price, int maxAmountInStock, int amountPerBox) : base(id, name, description, price, maxAmountInStock, amountPerBox)
         {
